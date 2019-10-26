b5cdec9f0bdcb2ff7742f95bcfd35bcde6d9ac87
coco <rmecila@gmail.com>
2019-10-26 22:27:17 +0900

    fixed rename class fixed  OrderRunner to RunActionsOrder

--- src/index.ts
+++ src/index.ts
@@ -17,33 +17,10 @@ export function isIAction<TParam, TResult>(arg: any): arg is IAction<TParam, TRe
 }
 
 /* ************************ */
-// abstract
-/* ************************ */
-
-export abstract class BOrderRunner<TParam, TResult, TExecutorResult> implements IAction<TParam, TResult> {
-    protected previous: ListRunner<TParam>;
-    protected executor: IAction<TParam, TExecutorResult>;
-    protected following: ListRunner<TParam>;
-
-    constructor(
-        previous: IAction<TParam | void, void | Promise<void>>[],
-        executor: IAction<TParam | TParam, TExecutorResult>,
-        following: IAction<TParam | void, void | Promise<void>>[]) {
-
-        this.previous = new ListRunner(previous);
-        this.executor = executor;
-        this.following = new ListRunner(following);
-    }
-
-    abstract execute(param: TParam): TResult;
-}
-
-
-/* ************************ */
 // class
 /* ************************ */
 
-export class FreeAction<TParam, TResult> implements IAction<TParam, TResult> {
+export class Free<TParam, TResult> implements IAction<TParam, TResult> {
     private func: (param: TParam) => TResult;
 
     constructor(func: (param: TParam) => TResult) {
@@ -55,10 +32,17 @@ export class FreeAction<TParam, TResult> implements IAction<TParam, TResult> {
     }
 }
 
-export class ListRunner<TParam> implements IAction<TParam, Promise<void>>{
-    private list: IAction<void | TParam, void | Promise<void>>[];
+/* ------------------------ */
+// RunActions
+/* ------------------------ */
 
-    constructor(list: IAction<void | TParam, void | Promise<void>>[]) {
+export type PromiseOrVoid = void | Promise<void>;
+
+
+export class RunActions<TParam> implements IAction<TParam, Promise<void>>{
+    private list: IAction<void | TParam, PromiseOrVoid>[];
+
+    constructor(list: IAction<void | TParam, PromiseOrVoid>[]) {
         this.list = list;
     }
 
@@ -73,20 +57,31 @@ export class ListRunner<TParam> implements IAction<TParam, Promise<void>>{
     }
 }
 
-export class SyncRunner<TParam, TResult> extends BOrderRunner<TParam, Promise<TResult>, TResult> {
+export class RunActionsOrder<TParam, TResult> implements IAction<TParam, Promise<TResult>> {
+    protected previous: RunActions<TParam>;
+    protected executing: IAction<TParam, TResult | Promise<TResult>>;
+    protected following: RunActions<TParam>;
+
+    constructor(
+        previous: IAction<TParam | void, PromiseOrVoid>[],
+        executing: IAction<TParam, TResult | Promise<TResult>>,
+        following: IAction<TParam | void, PromiseOrVoid>[]) {
+
+        this.previous = new RunActions(previous);
+        this.executing = executing;
+        this.following = new RunActions(following);
+    }
+
     async execute(param: TParam): Promise<TResult> {
+
         await this.previous.execute(param);
-        const result = this.executor.execute(param);
+
+        const promiseOrResult = this.executing.execute(param);
+        const result = (promiseOrResult instanceof Promise) ? await promiseOrResult : promiseOrResult;
+
         await this.following.execute(param);
         return result;
     }
 }
 
-export class AsyncRunner<TParam, TResult> extends BOrderRunner<TParam, Promise<TResult>, Promise<TResult>> {
-    async execute(param: TParam): Promise<TResult> {
-        await this.previous.execute(param);
-        const result = await this.executor.execute(param);
-        await this.following.execute(param);
-        return result;
-    }
-}
+/* ------------------------ */
--- test/test.ts
+++ test/test.ts
@@ -4,190 +4,219 @@ import * as tst from "./testobj";
 
 /* ------------------------ */
 async function testAsync(
-    action: drs.IAction<void, Promise<void>>,
-    resultLog: Array<string>) {
+    testParam: {
+        action: drs.IAction<void, Promise<void>>,
+        resultLog: Array<string>
+    }) {
 
     tst.log.msgs = [];
-    await action.execute();
-    assert.ok(JSON.stringify(tst.log.msgs) === JSON.stringify(resultLog));
+    await testParam.action.execute();
+    assert.ok(JSON.stringify(tst.log.msgs) === JSON.stringify(testParam.resultLog));
 }
 
 async function testParamAsync<TParam, TResult>(
-    action: drs.IAction<TParam, Promise<TResult>>,
-    param: TParam,
-    result: TResult,
-    resultLog: Array<string>) {
+    testParam: {
+        action: drs.IAction<TParam, Promise<TResult>>,
+        param: TParam,
+        result: TResult,
+        resultLog: Array<string>
+    }) {
 
     tst.log.msgs = [];
-    assert.ok(result === await action.execute(param));
-    assert.ok(JSON.stringify(tst.log.msgs) === JSON.stringify(resultLog));
+    assert.ok(testParam.result === await testParam.action.execute(testParam.param));
+    assert.ok(JSON.stringify(tst.log.msgs) === JSON.stringify(testParam.resultLog));
 }
 
 /* ------------------------ */
-describe("ListRunner", () => {
-    it("Sync Only", async () => await testAsync(
-        new drs.ListRunner([
-            new tst.SA(),
-            new tst.SB(),
-            new tst.SA()
-        ]),
-        [
-            "S_", "SA", "S_",
-            "S_", "SA", "S_",
-            "SB",
-            "S_", "SA", "S_",
-            "S_", "SA", "S_",
-        ]
-    ));
+describe("RunActions", () => {
+    it("Sync Only", async () =>
+        await testAsync({
+            action: new drs.RunActions([
+                new tst.SA(),
+                new tst.SB(),
+                new tst.SA()
+            ]),
+            resultLog: [
+                "S_", "SA", "S_",
+                "S_", "SA", "S_",
+                "SB",
+                "S_", "SA", "S_",
+                "S_", "SA", "S_",
+            ]
+        }));
 
-    it("Async Only", async () => await testAsync(
-        new drs.ListRunner([
-            new tst.AA(),
-            new tst.AB(),
-            new tst.AA()
-        ]),
-        [
-            "A_", "AA", "A_",
-            "A_", "AA", "A_",
-            "AB",
-            "A_", "AA", "A_",
-            "A_", "AA", "A_",
-        ]
-    ));
+    it("Async Only", async () =>
+        await testAsync({
+            action: new drs.RunActions([
+                new tst.AA(),
+                new tst.AB(),
+                new tst.AA()
+            ]),
+            resultLog: [
+                "A_", "AA", "A_",
+                "A_", "AA", "A_",
+                "AB",
+                "A_", "AA", "A_",
+                "A_", "AA", "A_",
+            ]
+        }));
 
-    it("Async, Sync Mixing", async () => await testAsync(
-        new drs.ListRunner([
-            new tst.SA(),
-            new tst.AB(),
-            new tst.SA()
-        ]),
-        [
-            "S_", "SA", "S_",
-            "A_", "AA", "A_",
-            "AB",
-            "A_", "AA", "A_",
-            "S_", "SA", "S_",
-        ]
-    ));
+    it("Async, Sync Mixing", async () =>
+        await testAsync({
+            action: new drs.RunActions([
+                new tst.SA(),
+                new tst.AB(),
+                new tst.SA()
+            ]),
+            resultLog: [
+                "S_", "SA", "S_",
+                "A_", "AA", "A_",
+                "AB",
+                "A_", "AA", "A_",
+                "S_", "SA", "S_",
+            ]
+        }));
 
-    it("in param", async () => await testParamAsync(
-        new drs.ListRunner([
-            new tst.SA(),
-            new tst.ParamAction(),
-            new tst.SA()
-        ]),
-        "param", undefined,
-        [
-            "S_", "SA", "S_",
-            "param",
-            "S_", "SA", "S_",
-        ]
-    ));
+    it("in param", async () =>
+        await testParamAsync({
+            action: new drs.RunActions([
+                new tst.SA(),
+                new tst.ParamAction(),
+                new tst.SA()
+            ]),
+            param: "param",
+            result: undefined,
+            resultLog: [
+                "S_", "SA", "S_",
+                "param",
+                "S_", "SA", "S_",
+            ]
+        }));
 });
 
 /* ------------------------ */
-describe("SyncRunner", () => {
-    it("Sync Only", async () => await testParamAsync(
-        new drs.SyncRunner(
-            [new tst.SA()],
-            new tst.SyncAction(),
-            [new tst.SA()]
-        ),
-        "xxx", "xxx",
-        [
-            "S_", "SA", "S_",
-            "xxx",
-            "S_", "SA", "S_",
-        ]
-    ));
+describe("RunActionsOrder", () => {
+    it("SyncAction Sync Only", async () =>
+        await testParamAsync({
+            action: new drs.RunActionsOrder(
+                [new tst.SA()],
+                new tst.SyncAction(),
+                [new tst.SA()]
+            ),
+            param: "xxx",
+            result: "xxx",
+            resultLog: [
+                "S_", "SA", "S_",
+                "xxx",
+                "S_", "SA", "S_",
+            ]
+        }));
 
-    it("Async Only", async () => await testParamAsync(
-        new drs.SyncRunner(
-            [new tst.AA()],
-            new tst.SyncAction(),
-            [new tst.AA()]
-        ),
-        "xxx", "xxx",
-        [
-            "A_", "AA", "A_",
-            "xxx",
-            "A_", "AA", "A_",
-        ]
-    ));
+    it("SyncAction Async Only", async () =>
+        await testParamAsync({
+            action: new drs.RunActionsOrder(
+                [new tst.AA()],
+                new tst.SyncAction(),
+                [new tst.AA()]
+            ),
+            param: "xxx",
+            result: "xxx",
+            resultLog: [
+                "A_", "AA", "A_",
+                "xxx",
+                "A_", "AA", "A_",
+            ]
+        }));
 
-    it("Async, Sync Mixing", async () => await testParamAsync(
-        new drs.SyncRunner(
-            [new tst.AA(), new tst.SA()],
-            new tst.SyncAction(),
-            [new tst.SA(), new tst.AA()]
-        ),
-        "xxx", "xxx",
-        [
-            "A_", "AA", "A_",
-            "S_", "SA", "S_",
-            "xxx",
-            "S_", "SA", "S_",
-            "A_", "AA", "A_",
-        ]
-    ));
+    it("SyncAction Async, Sync Mixing", async () =>
+        await testParamAsync({
+            action: new drs.RunActionsOrder(
+                [
+                    new tst.AA(),
+                    new tst.SA()
+                ],
+                new tst.SyncAction(),
+                [
+                    new tst.SA(),
+                    new tst.AA()
+                ]
+            ),
+            param: "xxx",
+            result: "xxx",
+            resultLog: [
+                "A_", "AA", "A_",
+                "S_", "SA", "S_",
+                "xxx",
+                "S_", "SA", "S_",
+                "A_", "AA", "A_",
+            ]
+        }));
+
+    it("AsyncAction Sync Only", async () =>
+        await testParamAsync({
+            action: new drs.RunActionsOrder(
+                [new tst.SA()],
+                new tst.AsyncAction(),
+                [new tst.SA()]
+            ),
+            param: "xxx",
+            result: "xxx",
+            resultLog: [
+                "S_", "SA", "S_",
+                "xxx",
+                "S_", "SA", "S_",
+            ]
+        }));
+
+    it("AsyncAction Async Only", async () =>
+        await testParamAsync({
+            action: new drs.RunActionsOrder(
+                [new tst.AA()],
+                new tst.AsyncAction(),
+                [new tst.AA()]
+            ),
+            param: "xxx",
+            result: "xxx",
+            resultLog: [
+                "A_", "AA", "A_",
+                "xxx",
+                "A_", "AA", "A_",
+            ]
+        }));
+
+    it("AsyncAction Async, Sync Mixing", async () =>
+        await testParamAsync({
+            action: new drs.RunActionsOrder(
+                [
+                    new tst.AA(),
+                    new tst.SA()
+                ],
+                new tst.AsyncAction(),
+                [
+                    new tst.SA(),
+                    new tst.AA()
+                ]
+            ),
+            param: "xxx",
+            result: "xxx",
+            resultLog: [
+                "A_", "AA", "A_",
+                "S_", "SA", "S_",
+                "xxx",
+                "S_", "SA", "S_",
+                "A_", "AA", "A_",
+            ]
+        }));
 });
 
-/* ------------------------ */
-describe("AsyncRunner", () => {
-    it("Sync Only", async () => await testParamAsync(
-        new drs.AsyncRunner(
-            [new tst.SA()],
-            new tst.AsyncAction(),
-            [new tst.SA()]
-        ),
-        "xxx", "xxx",
-        [
-            "S_", "SA", "S_",
-            "xxx",
-            "S_", "SA", "S_",
-        ]
-    ));
-
-    it("Async Only", async () => await testParamAsync(
-        new drs.AsyncRunner(
-            [new tst.AA()],
-            new tst.AsyncAction(),
-            [new tst.AA()]
-        ),
-        "xxx", "xxx",
-        [
-            "A_", "AA", "A_",
-            "xxx",
-            "A_", "AA", "A_",
-        ]
-    ));
-
-    it("Async, Sync Mixing", async () => await testParamAsync(
-        new drs.AsyncRunner(
-            [new tst.AA(), new tst.SA()],
-            new tst.AsyncAction(),
-            [new tst.SA(), new tst.AA()]
-        ),
-        "xxx", "xxx",
-        [
-            "A_", "AA", "A_",
-            "S_", "SA", "S_",
-            "xxx",
-            "S_", "SA", "S_",
-            "A_", "AA", "A_",
-        ]
-    ));
-});
 
 /* ------------------------ */
-describe("FreeAction", () => {
-    it("success", () => {
-        let isCall = false;
-
-        new drs.FreeAction<void, void>(() => {
-            isCall = true;
-        }).execute();
-
-        assert.ok(isCall);
-    });
+describe("Free", () => {
+    it("success", async () =>
+        await testParamAsync({
+            action: new drs.Free<string, Promise<string>>(async (param) => param),
+            param: "xxx",
+            result: "xxx",
+            resultLog: []
+        }));
 });
